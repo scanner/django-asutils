5b21b83b4486e375930e742bb02bb0125cb2b4a9
Scanner <scanner@apricot.com>
2013-04-25 20:17:23 -0700

    Copy the old django generic simple redirect_to and direct_to_template views so we can get old apps up and running before converting them to use a better method.

--- asutils/views.py
+++ asutils/views.py
@@ -8,15 +8,67 @@ import os.path
 
 # Django imports
 #
-from django.views.generic.simple import direct_to_template
 from django.utils import simplejson
-from django.http import HttpResponse, Http404
+from django.template import loader, RequestContext
+from django.http import HttpResponse, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseGone
 from django.utils.functional import Promise
-from django.utils.translation import force_unicode
-from django.utils.simplejson import JSONEncoder
+from django.utils.encoding import force_unicode
 from django.conf import settings
 
 from asutils.middleware import allow_anonymous
+
+##
+## We copy direct_to_template() and redirect_to() views from an
+## earlier version of django.views.generic.simple so we can continue
+## using it in some of our utilities until we do a proper replacement
+## of them.
+##
+
+####################################################################
+#
+def direct_to_template(request, template, extra_context=None, mimetype=None,
+                       **kwargs):
+    """
+    Render a given template with any extra URL parameters in the context as
+    ``{{ params }}``.
+    """
+    if extra_context is None: extra_context = {}
+    dictionary = {'params': kwargs}
+    for key, value in extra_context.items():
+        if callable(value):
+            dictionary[key] = value()
+        else:
+            dictionary[key] = value
+    c = RequestContext(request, dictionary)
+    t = loader.get_template(template)
+    return HttpResponse(t.render(c), mimetype=mimetype)
+
+####################################################################
+#
+def redirect_to(request, url, permanent=True, **kwargs):
+    """
+    Redirect to a given URL.
+
+    The given url may contain dict-style string formatting, which will be
+    interpolated against the params in the URL.  For example, to redirect from
+    ``/foo/<id>/`` to ``/bar/<id>/``, you could use the following URLconf::
+
+        urlpatterns = patterns('',
+            ('^foo/(?P<id>\d+)/$', 'django.views.generic.simple.redirect_to', {'url' : '/bar/%(id)s/'}),
+        )
+
+    If the given url is ``None``, a HttpResponseGone (410) will be issued.
+
+    If the ``permanent`` argument is False, then the response will have a 302
+    HTTP status code. Otherwise, the status code will be 301.
+    """
+    if url is not None:
+        klass = permanent and \
+            HttpResponsePermanentRedirect or \
+            HttpResponseRedirect
+        return klass(url % kwargs)
+    else:
+        return HttpResponseGone()
 
 ####################################################################
 #
