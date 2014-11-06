253778216c6e5a430e129e8027950d74a8f8b2e1
Scanner <scanner@apricot.com>
2014-11-05 19:14:15 -0800

    Update version to match our release version

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from distutils.command.install import INSTALL_SCHEMES
 # Tell distutils to put the data_files in platform-specific installation
 # locations. See here for an explanation:
 # http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+#
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
 
 # Dynamically calculate the version based on asutils.VERSION.
+#
 version_tuple = __import__('asutils').VERSION
 if version_tuple[2] is not None:
     version = "%d.%d_%s" % version_tuple
